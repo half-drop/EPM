118dce0e5828c5b00ff7e6bc0e7c08b5ad6247be
BuildTools <unconfigured@null.spigotmc.org>
2024-07-02 11:16:46 +0800

    添加项目文件。

---
+++ EPM/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using EPM.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace EPM.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EPMEntities db = new EPMEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return Content("<script>alert('账号密码不能为空');location.href='Login';</script>", "text/html");
+            }
+
+            var user = db.Employees.Where(u => u.Username == username && u.Password == password).SingleOrDefault();
+
+            if (user == null)
+            {
+                return Content("<script>alert('输入有误');location.href='Login';</script>", "text/html");
+            }
+            else
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+                TempData["Username"] = username;
+                TempData["UserRole"] = user.Role;
+                return RedirectToAction("Dashboard", "Employees");
+            }
+        }
+    }
+}
---
+++ EPM/Views/NotificationsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EPM.Models;
+
+namespace EPM.Views
+{
+    public class NotificationsController : Controller
+    {
+        private EPMEntities db = new EPMEntities();
+
+        // GET: Notifications
+        public ActionResult Index()
+        {
+            return View(db.Notifications.ToList());
+        }
+
+        // GET: Notifications/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Notifications notifications = db.Notifications.Find(id);
+            if (notifications == null)
+            {
+                return HttpNotFound();
+            }
+            return View(notifications);
+        }
+
+        // GET: Notifications/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Notifications/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性。有关
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NotificationID,Title,Content,DatePublished")] Notifications notifications)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Notifications.Add(notifications);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(notifications);
+        }
+
+        // GET: Notifications/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Notifications notifications = db.Notifications.Find(id);
+            if (notifications == null)
+            {
+                return HttpNotFound();
+            }
+            return View(notifications);
+        }
+
+        // POST: Notifications/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性。有关
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "NotificationID,Title,Content,DatePublished")] Notifications notifications)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(notifications).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(notifications);
+        }
+
+        // GET: Notifications/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Notifications notifications = db.Notifications.Find(id);
+            if (notifications == null)
+            {
+                return HttpNotFound();
+            }
+            return View(notifications);
+        }
+
+        // POST: Notifications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Notifications notifications = db.Notifications.Find(id);
+            db.Notifications.Remove(notifications);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
